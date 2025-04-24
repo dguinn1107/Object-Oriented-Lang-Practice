5d128ffd3b0ef7334135accb0783267354a18ed4
Daigeona Guinn <dguinn1107@gmail.com>
2025-04-24 13:45:05 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿// See https://aka.ms/new-console-template for more information
+using OOPReview;
+
+public static class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Welcome to Daisy's Car Rental! What Vehicle Would You Like To Rent? (Car, Truck, Motorcycle)");
+
+        string VehicleType = Console.ReadLine();
+
+        if (VehicleType == "Car")
+        {
+            Vehicle car = new Car();
+            Console.WriteLine("How Many Days Would You Like To Rent The Vehicle For?");
+            int RentalDays = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("The Total Cost Of Your Rental Is: " + car.CalculateRentalCost(RentalDays));
+        }
+        else if (VehicleType == "Truck")
+        {
+            Vehicle truck = new Truck();
+            Console.WriteLine("How Many Days Would You Like To Rent The Vehicle For?");
+            int RentalDays = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("The Total Cost Of Your Rental Is: " + truck.CalculateRentalCost(RentalDays));
+        }
+        else if (VehicleType == "Motorcycle")
+        {
+            Vehicle motorcycle = new Motorcycle();
+            Console.WriteLine("How Many Days Would You Like To Rent The Vehicle For?");
+            int RentalDays = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("The Total Cost Of Your Rental Is: " + motorcycle.CalculateRentalCost(RentalDays));
+        }
+        else
+        {
+            Console.WriteLine("Invalid Vehicle Type");
+        }
+    }
+}
---
+++ Vehicle.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPReview
+{
+    //abstraction practice
+    abstract public class Vehicle
+    {
+        private string Make { get; }
+        private string Model { get; }
+        private int Year { get; }
+
+        public abstract decimal CalculateRentalCost(int RentalDays);
+
+        public string make
+        {
+            get
+            {
+                return Make;
+            }
+        }
+        public string model
+        {
+            get
+            {
+                return Model;
+            }
+        }
+        public int year
+        {
+            get
+            {
+                return Year;
+            }
+        }
+    }
+
+    public class Car : Vehicle
+    {
+        public override decimal CalculateRentalCost(int RentalDays)
+        {
+            return RentalDays * 50;
+        }
+    }
+
+    public class Truck : Vehicle
+    {
+        public override decimal CalculateRentalCost(int RentalDays)
+        {
+            if (RentalDays <= 3)
+            {
+                return RentalDays * 100;
+            }
+            else
+            {
+                int newrentaldays = RentalDays - 3;
+                decimal Basecost = 300;
+
+                return newrentaldays * 120 + Basecost;
+            }
+        }
+    }
+
+    public class Motorcycle : Vehicle
+    {
+        public override decimal CalculateRentalCost(int RentalDays)
+        {
+            return RentalDays * 15;
+        }
+    }
+
+
+    }
